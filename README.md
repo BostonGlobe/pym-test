dc1ae3d262d8e2dc1764880508ea8ab6281f2b45
russellgoldenberg <russellgoldenberg@gmail.com>
2016-05-19 08:49:55 -0400

    resize

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Run `make reload`
 
 This is using raf polling for parent width. Of note: the parent can send a width message(s) before the child is even setup to receive messages.
 
+* You can toggle the resizeChart function in src/main.js (which event triggers it)
 * See the bottom of preview.html (automatically resizing twice on timeouts)
 * See src/main.js for resize event handling
 * See bottom of src/index.html for iframe setup
--- src/main.js
+++ src/main.js
@@ -2,18 +2,25 @@
 (function() {
 	// global variables
 
+	var el = document.querySelector('.chart')
 
+	var resizeChart = function(w) {
+		el.style.width = w + 'px'
+	}
 
 	// called once on page load
 	var init = function() {
 		window.addEventListener('resize', function() {
-			console.log('[child] resize:', window.innerWidth)
+			var width =  window.innerWidth
+			console.log('[child] resize:', width)
+			// resizeChart(width)
 		})
 	};
 
 	// called automatically on article page resize
 	window.onResize = function(width) {
 		console.log('[child] renderCallback:', width)
+		resizeChart(width)
 	};
 
 	// graphic code
